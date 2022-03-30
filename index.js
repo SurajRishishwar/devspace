e86cf72867009fb19149488b032001495b08b8d2
Suraj Sharma <75289779+SurajRishishwar@users.noreply.github.com>
2022-03-30 23:23:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const cookieParser=require('cookie-parser');
 const app = express();
-const port= process.env.port || 8000;
+const port= process.env.PORT || 8000;
 const db=require('./config/mongoose');
 const googlestt=require('./config/passport-google-oauth2-strategy');
 const passport = require('passport');
